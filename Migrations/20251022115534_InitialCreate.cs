9e918bcb16c03085cc19ac8536a4b29534c89888
Lorenzo Pedrozo <lorenzopedrozo1106@gmail.com>
2025-10-22 09:02:20 -0300

    Primeira vez testando o codigo

---
+++ Migrations/20251022115534_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APITAREFAS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projetos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true),
+                    Responsavel = table.Column<string>(type: "TEXT", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataFimPrevista = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ProgressoPercentual = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projetos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projetos");
+        }
+    }
+}
